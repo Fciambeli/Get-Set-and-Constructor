a8102841d6917713d3ca77f187cbdbfe0452b24a
Fciambeli <fciambeli@gmail.com>
2023-03-29 07:00:48 -0300

    Get and Set.

---
+++ src/getset/GetSet.java
@@ -0,0 +1,10 @@
+package getset;
+public class GetSet {   
+    public static void main(String[] args) {
+      Caneta c1 = new Caneta("Mont Blanc", "Verde", 0.4f);
+      c1.status();
+      Caneta c2 = new Caneta ("Crystal", "Vermelho", 1.5f);
+      c2.status();
+    }
+    
+}
